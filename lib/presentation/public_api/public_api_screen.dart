f9dda4fe1409efaeb68ac0c04492bac7659aafb5
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-05-26 17:10:27 +0600

    feat: design ui and show data

---
+++ lib/presentation/public_api/public_api_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:network_calling/presentation/public_api/cubits/public_api/cubit.dart';
+import 'package:network_calling/presentation/public_api/cubits/public_api/state.dart';
+
+class PublicApiListScreen extends StatelessWidget {
+  const PublicApiListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All APIs'),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<PublicApiCubit, PublicApiState>(
+        builder: (context, state) {
+          if (state is ApiFetchLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is ApiFetchError) {
+            return Center(
+              child: Text(state.errorMessage),
+            );
+          }
+          if (state is ApiFetchLoaded) {
+            return ListView.builder(
+              itemBuilder: (ctx, index) => ListTile(
+                title: Text(state.entry[index].api),
+                subtitle: Text(state.entry[index].description),
+              ),
+              itemCount: state.entry.length,
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
