6b78ca582a2cf57c45af3ae3006c7e64175e244d
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-05-31 22:20:39 +0600

    fix: remove warning

--- lib/data/model/public_api/public_api_response.dart
+++ lib/data/model/public_api/public_api_response.dart
@@ -37,83 +37,16 @@ class PublicApiResponse with _$PublicApiResponse {
 class Entry with _$Entry {
   const factory Entry({
     // ignore: invalid_annotation_target
-    @JsonKey(name: "API") required String api,
-    @JsonKey(name: "Description") @Default("defaultValue") String description,
+    @JsonKey(name: 'API') required String api,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'Description') @Default('Empty') String description,
     String? auth,
     bool? https,
     String? cors,
-    @JsonKey(name: "Link") @Default("Empty") String link,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'Link') @Default('Empty') String link,
     String? category,
   }) = _Entry;
 
   factory Entry.fromJson(Map<String, dynamic> json) => _$EntryFromJson(json);
 }
-/*
-class PublicApiEntity {
-  int? count;
-  List<Entries>? entries;
-
-  PublicApiEntity({this.count, this.entries});
-
-  PublicApiEntity.fromJson(Map<String, dynamic> json) {
-    count = json['count'] as int;
-    if (json['entries'] != null) {
-      entries = <Entries>[];
-      json['entries'].forEach((v) {
-        entries!.add(new Entries.fromJson(v as Map<String, dynamic>));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['count'] = this.count;
-    if (this.entries != null) {
-      data['entries'] = this.entries!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
-class Entries {
-  String? aPI;
-  String? description;
-  String? auth;
-  bool? hTTPS;
-  String? cors;
-  String? link;
-  String? category;
-
-  Entries(
-      {this.aPI,
-      this.description,
-      this.auth,
-      this.hTTPS,
-      this.cors,
-      this.link,
-      this.category});
-
-  Entries.fromJson(Map<String, dynamic> json) {
-    aPI = json['API'] as String;
-    description = json['Description'] as String;
-    auth = json['Auth'] as String;
-    hTTPS = json['HTTPS'] as bool;
-    cors = json['Cors'] as String;
-    link = json['Link'] as String;
-    category = json['Category'] as String;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['API'] = this.aPI;
-    data['Description'] = this.description;
-    data['Auth'] = this.auth;
-    data['HTTPS'] = this.hTTPS;
-    data['Cors'] = this.cors;
-    data['Link'] = this.link;
-    data['Category'] = this.category;
-    return data;
-  }
-}
-
- */
