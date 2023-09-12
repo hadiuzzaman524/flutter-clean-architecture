58c00fa72647c6199e0bedeff7b9aa13eea493ed
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-09-12 22:03:42 +0600

    chore: add network module

--- lib/data/data_sources/public_api/remote_data_sources.dart
+++ lib/data/data_sources/public_api/remote_data_sources.dart
@@ -1,12 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:network_calling/data/response_objects/public_api/public_api_response.dart';
 import 'package:retrofit/http.dart';
 
 part 'remote_data_sources.g.dart';
 
 @RestApi()
+@lazySingleton
 abstract class RemoteDataSources {
-  factory RemoteDataSources(Dio dio) = _RemoteDataSources;
+  @factoryMethod
+  factory RemoteDataSources(@Named('unauthenticated') Dio dio) =
+      _RemoteDataSources;
 
   @GET('/entries')
   Future<PublicApiResponse> getAllApi();
--- lib/data/repository/public_api/public_api_repository_impl.dart
+++ lib/data/repository/public_api/public_api_repository_impl.dart
@@ -3,17 +3,13 @@ import 'package:network_calling/data/data_sources/public_api/remote_data_sources
 import 'package:network_calling/data/repository/re_mapper/api_remapper.dart';
 import 'package:network_calling/domain/entities/api/api_entity.dart';
 import 'package:network_calling/domain/services/public_api/public_api_services.dart';
-import 'package:network_calling/services/dio_services.dart';
 
 @Injectable(as: PublicApiServices)
 class PublicApiImpl implements PublicApiServices {
-  PublicApiImpl(this._dioService, this._apiReMapper) {
-    _remoteDataSource = RemoteDataSources(_dioService.dio);
-  }
+  PublicApiImpl(this._apiReMapper, this._remoteDataSource);
 
-  late RemoteDataSources _remoteDataSource;
+  final RemoteDataSources _remoteDataSource;
   final ApiReMapper _apiReMapper;
-  final DioService _dioService;
 
   @override
   Future<List<ApiEntity>> getAllApi() async {
